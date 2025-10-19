46fa7bb429cac7b342025416024d7f1e4ac743c2
Devadathan M B <devadathanmb@gmail.com>
2025-10-19 11:33:01 +0530

    fix: Fix broken reading time on blogs

--- src/utils/remark-reading-time.mjs
+++ src/utils/remark-reading-time.mjs
@@ -2,9 +2,18 @@ import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 
 export function remarkReadingTime() {
-  return function (tree, { data }) {
+  return function (tree, file) {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
-    data.astro.frontmatter.readingTime = readingTime.text;
+
+    // Ensure data.astro exists
+    if (!file.data.astro) {
+      file.data.astro = {};
+    }
+    if (!file.data.astro.frontmatter) {
+      file.data.astro.frontmatter = {};
+    }
+
+    file.data.astro.frontmatter.readingTime = readingTime.text;
   };
 }
