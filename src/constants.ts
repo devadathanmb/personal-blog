8142d336ff9b0cc9b330a60bfd44fcdbbd52e0ed
Devadathan M B <devadathanmb@gmail.com>
2025-12-30 14:28:48 +0530

    feat: Update site configuration (title, author, socials)

--- src/constants.ts
+++ src/constants.ts
@@ -19,27 +19,27 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/devadathanmb",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
-  },
-  {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://linkedin.com/in/devadathan-m-b-804876257",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:devadathanmb@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
+  },
+  {
+    name: "Telegram",
+    href: "https://telegram.me/devOnTelegram",
+    linkTitle: `${SITE.title} on Telegram`,
+    icon: IconTelegram,
   },
 ] as const;
 
