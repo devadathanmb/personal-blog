ca661c8ecfc9059e384fb070a6b2cab50c78c8ce
Devadathan M B <devadathanmb@gmail.com>
2026-01-01 17:21:04 +0530

    refactor: remove barrel export for terminal components
    Remove index.ts barrel export that was causing TypeScript LSP errors
    when re-exporting .astro components from .ts files

--- src/components/terminal/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as HeroTerminal } from "./HeroTerminal.astro";
-export { default as TerminalLine } from "./TerminalLine.astro";
-export { default as TerminalOutput } from "./TerminalOutput.astro";
