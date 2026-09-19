a29b360c8bcb8c03de2818546142e4531f979da2
Devadathan M B <devadathanmb@gmail.com>
2026-09-19 18:50:53 +0530

    chore: use dot background everywhere

--- src/types.ts
+++ src/types.ts
@@ -472,7 +472,7 @@ export interface Ui {
 }
 
 /* FEATURES */
-export type BgType = 'plum' | 'dot' | 'rose' | 'particle' | 'ocean' | 'storm'
+export type BgType = 'plum' | 'dot' | 'rose' | 'particle' | 'storm'
 export type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6
 type Mentioned = `@${string}` | `@${string}@${string}` | ''
 type FeatureConfig<T> = false | [boolean, T]
